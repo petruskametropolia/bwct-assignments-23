3913f979825671a9a29c476100da9170806e09da
Petrus Kajastie <petrus.kajastie@metropolia.fi>
2023-03-15 11:25:56 +0200

    pug aloitus

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ const port = 3000
 
 app.use(express.static('public'));
 
+app.set('view engine', 'pug');
 
+app.get('/', (req,res) => {
+    const username = 'Käyttähän nimi';
+    const description = 'Aku ankka';
+    const content = {title: username, description};
+res.render('index', content);
+});
 
 app.get('/kukkuu', (req, res) => {
     res.send('No moro')
+  })
+
+  app.get('/esim-sivu', (req, res) => {
+    const monkDataFromD8 = 'Käyttähän nimi';
+    const response = '<html><head><title></head></body></html>';
+    res.send(response);
   })
 
   app.get("/catinfo", (req, res) => {
