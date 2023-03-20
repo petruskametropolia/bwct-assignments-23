489cc94c974dfbba76875231b0cd04b6722b2541
Petrus Kajastie <petrus.kajastie@metropolia.fi>
2023-03-20 11:33:06 +0200

    aamupäivän tunti

--- controllers/catController.js
+++ controllers/catController.js
@@ -1,2 +1,23 @@
 'use strict';
 // catController
+const catModel = require('../models/catModel');
+
+const cats = catModel.cats;
+
+const getCatList = (req, res) => {
+    res.json(cats);
+    //res.send('From this endpoint you can get cats.');
+};
+
+const getCat = (req, res) => {
+    //console.log(req.params);
+    const id = req.params.catId;
+    //todo, filter matching cat based on id
+    // todo , response 404 if id not found in array
+    const cat = cats[0];
+    res.json(cat);
+   // res.send('From this endpoint you can get a cat with id: ' + req.params.catId);
+  }
+
+const catController = {getCatList, getCat};
+module.exports = catController;
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,23 @@
 'use strict';
 // catRoute
+const express = require('express');
+const router = express.Router();
+const catController = require('../controllers/catController');
+
+//Todo move functions to controller
+router.get('/', catController.getCatList);
+  router.get('/:catId',catController.getCat);
+  
+  router.post('/', (req, res) => {
+    res.send('From this endpoint you can add cats.')
+  });
+  
+  router.put('/', (req, res) => {
+    res.send('From this endpoint you can modify cats.')
+  });
+  
+  router.delete('/', (req, res) => {
+    res.send('From this endpoint you can delete cats.')
+  });
+
+module.exports = router;
