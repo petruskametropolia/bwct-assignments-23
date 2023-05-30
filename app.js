67e6d751326dd3f7f5d4c617b49592112adbec23
Petrus Kajastie <petrus.kajastie@metropolia.fi>
2023-05-30 03:03:49 +0300

    login signup ja pet upload toimii

--- app.js
+++ app.js
@@ -4,7 +4,83 @@ const cors = require('cors');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
 const app = express();
+const multer = require('multer');
 const port = 3000;
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const session = require('express-session');
+
+
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true
+  }));
+  
+  app.use(flash());
+
+  const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'dbuser',
+    password: 'test123',
+    database: 'catdb'
+  });
+
+  db.connect((err) => {
+    if (err) throw err;
+   // console.log('Connected to database!');
+  });
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static('public')); 
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      req.flash('error', 'Please provide both username and password.');
+      return res.redirect('/ui6/login.html');
+    }
+  
+    const sql = `SELECT * FROM wop_user WHERE name = ? AND password = ?`;
+  
+    db.query(sql, [username, password], (err, results) => {
+      if (err) {
+        console.error(err);
+        req.flash('error', 'Internal Server Error');
+        return res.redirect('/ui6/login.html');
+      }
+  
+      if (results.length > 0) {
+        // Login successful, redirect to desired page
+        return res.redirect('/ui2/front.html');
+      } else {
+        req.flash('error', 'Incorrect username or password.');
+  
+        // Execute client-side JavaScript to show pop-up message
+        const script = `
+          <script>
+            alert('Incorrect username or password.');
+            window.location.href = '/ui6/login.html';
+          </script>
+        `;
+  
+        return res.send(script);
+      }
+    });
+  });
+
+app.post('/signup', (req, res) => {
+    const { username,email, password } = req.body;
+    const sql = `INSERT INTO wop_user (name,email, password) VALUES (?, ?, ?)`;
+  
+    db.query(sql, [username,email, password], (err, results) => {
+      if (err) throw err;
+  
+    //  res.send('User created successfully!');
+      res.redirect('/ui2/front.html');
+    });
+  });
 
 app.use((req, res, next) =>{
     console.log(Date.now() + ': request: ' + req.method + '' + req.path)
